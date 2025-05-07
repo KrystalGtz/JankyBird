5b7fbd2a99e38ca54d2db256c6d67d9f202ff6a6
KrystalGtz <krystaljadegutierrez@gmail.com>
2025-05-07 14:09:32 -0500

    Main mechanics done

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -6,22 +6,30 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrength = 10;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
 
     // Start is called before the first frame update
     // starts as soon as the script is enabled, runs once
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     // runs constantly while the script is enabled
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
